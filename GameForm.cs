50f4a8d11405b10ee9de8e4e1bad8b43dd691be9
Jeffrey Richards <jmidrichards@gmail.com>
2020-12-04 00:08:37 -0500

    Initial commit

---
+++ GameForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project5
+{
+	public partial class GameForm : Form
+	{
+		public GameForm()
+		{
+			InitializeComponent();
+		}
+
+		private void GameForm_Load(object sender, EventArgs e)
+		{
+			this.Text = $"{Application.ProductName} by {Application.CompanyName}";
+			StreamReader sr = new StreamReader(@"..\..\states.txt");
+
+		}
+	}
+}
